9d4c553bb5ada418a44613a9348a9317637b4aac
William Chen <57119977+OCWC22@users.noreply.github.com>
2022-12-03 03:43:09 -0800

    services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DemoService } from './demo.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-architecture';
+  constructor(public demoService: DemoService) {}
 }
+
